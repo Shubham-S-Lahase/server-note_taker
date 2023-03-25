de0c0faebe1e3e4c929e49dd854cf764ba482c63
Shubham <shubhamlahase@hmail.com>
2023-03-25 10:32:13 +0530

    express server created and testes

---
+++ index.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express  = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+let port = process.env.PORT;
+let url = process.env.URL;
+
+mongoose.set("strictQuery", true);
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+    console.log("Connected to DB");
+})
+.catch((err) => {
+    console.log("Error Connecting to database", err);
+})
+
+app.get("/test", (req,res) => {
+    res.json("Express Server Test => OK");
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+})
